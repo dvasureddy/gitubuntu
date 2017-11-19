51b72676d6a449ffddcbf880499487dacb3355ea
suprajareddy <vasuforsuppu@gmail.com>
2017-11-19 05:04:17 +0000

    commit for book1.java

---
+++ book1.java
@@ -0,0 +1,9 @@
+need to do practice for this
+we have to get stuff to write a book
+
+thisnk encap:Ethernet  HWaddr 00:06:4f:4a:66:f0
+    BROADCAST MULTICAST  MTU:1500  Metric:1
+    RX packets:0 errors:0 dropped:0 overruns:0 frame:0
+    TX packets:0 errors:0 dropped:0 overruns:0 carrier:0
+    collisions:0 txqueuelen:1000
+    RX bytes:0 (0.0 B)  TX bytes:0 (0.0 B) is for book for learn git
